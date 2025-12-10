e2af94a1c18ab0d4431c19d4f04f176ebea9f421
Lyn Aguayo <lyn.aguayoc@gmail.com>
2025-12-10 21:34:23 +0100

    Define action for button B

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+my_number = 0
+# configurar grup de radio
+radio.set_group(1)
+# inici
+basic.show_icon(IconNames.HEART)
 
 # boto A: enviar missatge de text
 
@@ -7,3 +12,17 @@ def on_button_pressed_a():
     basic.pause(500)
     basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# botó B: jugar als daus (enviar número aleatori)
+
+def on_button_pressed_b():
+    global my_number
+    my_number = randint(1, 6)
+    # enviar el meu número
+    radio.send_number(my_number)
+    # mostrar el meu número
+    basic.show_string("JO:")
+    basic.show_number(my_number)
+    basic.pause(1000)
+    basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
+let my_number = 0
+//  configurar grup de radio
+radio.setGroup(1)
+//  inici
+basic.showIcon(IconNames.Heart)
 //  boto A: enviar missatge de text
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     radio.sendString("Hola!!")
     basic.showIcon(IconNames.Yes)
     basic.pause(500)
+    basic.clearScreen()
+})
+//  botó B: jugar als daus (enviar número aleatori)
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    my_number = randint(1, 6)
+    //  enviar el meu número
+    radio.sendNumber(my_number)
+    //  mostrar el meu número
+    basic.showString("JO:")
+    basic.showNumber(my_number)
+    basic.pause(1000)
     basic.clearScreen()
 })
